f6b8ef39515e221eef95c048cac8e93c88a6a9a3
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2025-01-11 13:13:22 +0200

    Update

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -10,18 +10,19 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://okaetsu.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Okaetsu', // Usually your GitHub org/user name.
-  projectName: 'PalSchema', // Usually your repo name.
+  organizationName: 'okaetsu', // Usually your GitHub org/user name.
+  projectName: 'okaetsu.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
