f82770465406296c329466269f3555b056876b32
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2025-01-13 08:24:47 +0200

    Update MonsterModLoader header

--- include/Loader/PalMonsterModLoader.h
+++ include/Loader/PalMonsterModLoader.h
@@ -5,43 +5,43 @@
 #include "nlohmann/json.hpp"
 
 namespace UECustom {
-	class UDataTable;
+    class UDataTable;
 }
 
 namespace Palworld {
-	class PalMonsterModLoader : public PalModLoaderBase {
-	public:
-		PalMonsterModLoader();
+    class PalMonsterModLoader : public PalModLoaderBase {
+    public:
+        PalMonsterModLoader();
 
-		~PalMonsterModLoader();
+        ~PalMonsterModLoader();
 
-		void Initialize();
-		
-		virtual void Load(const nlohmann::json& json) override final;
-	private:
-		void Add(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
+        void Initialize();
+        
+        virtual void Load(const nlohmann::json& json) override final;
+    private:
+        void Add(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
 
-		void Edit(uint8_t* TableRow, const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
+        void Edit(uint8_t* TableRow, const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
 
-		void AddIcon(const RC::Unreal::FName& CharacterId, const RC::StringType& IconPath);
+        void AddIcon(const RC::Unreal::FName& CharacterId, const RC::StringType& IconPath);
 
-		void AddBlueprint(const RC::Unreal::FName& CharacterId, const RC::StringType& BlueprintPath);
+        void AddBlueprint(const RC::Unreal::FName& CharacterId, const RC::StringType& BlueprintPath);
 
-		void AddAbilities(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
+        void AddAbilities(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
 
-		void AddLoot(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
+        void AddLoot(const RC::Unreal::FName& CharacterId, const nlohmann::json& properties);
 
-		void AddTranslations(const RC::Unreal::FName& CharacterId, const nlohmann::json& Data);
+        void AddTranslations(const RC::Unreal::FName& CharacterId, const nlohmann::json& Data);
 
-		void EditTranslations(const RC::Unreal::FName& CharacterId, const nlohmann::json& Data);
+        void EditTranslations(const RC::Unreal::FName& CharacterId, const nlohmann::json& Data);
 
-		UECustom::UDataTable* m_dataTable;
-		UECustom::UDataTable* m_iconDataTable;
-		UECustom::UDataTable* m_palBpClassTable;
-		UECustom::UDataTable* m_wazaMasterLevelTable;
-		UECustom::UDataTable* m_palDropItemTable;
-		UECustom::UDataTable* m_palNameTable;
-		UECustom::UDataTable* m_palShortDescTable;
-		UECustom::UDataTable* m_palLongDescTable;
-	};
+        UECustom::UDataTable* m_dataTable;
+        UECustom::UDataTable* m_iconDataTable;
+        UECustom::UDataTable* m_palBpClassTable;
+        UECustom::UDataTable* m_wazaMasterLevelTable;
+        UECustom::UDataTable* m_palDropItemTable;
+        UECustom::UDataTable* m_palNameTable;
+        UECustom::UDataTable* m_palShortDescTable;
+        UECustom::UDataTable* m_palLongDescTable;
+    };
 }
